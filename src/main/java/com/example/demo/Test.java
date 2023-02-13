8e0f5d77e3b8832be00809507e19b3fef16f0c30
Yao <yaoxiangwen@outlook.com>
2023-02-13 21:14:47 +0800

    第二次提交

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println("fsdafas");
+    }
+}
